defeb19bc2e091d35b57d8664263186f708efb06
caner.borekci <caner.borekci@CANERBOREKCI.etiyacorp.local>
2021-11-10 23:42:50 +0300

    loto

--- homework/src/homework/Homework1.java
+++ homework/src/homework/Homework1.java
@@ -11,29 +11,32 @@ public class Homework1 {
 
 		for (int i = 0; i < kolonSayisi; i++) {
 			System.out.print(i + 1 + ". Kolon = ");
+			//rastgele sayı üretiyoruz
 			for (int j = 0; j < loto.length; j++) {
 				int sayi = 1 + random.nextInt(49);
+				//en az iki basamak üretildiğinde eşitlik durumuna bakıyoruz var ise yeni üretiyoruz
 				for (int x = 0; x < j; x++) {
 					if (loto[x] == sayi) {
 						sayi = 1 + random.nextInt(49);
 					}
 				}
 				loto[j] = sayi;
-				
+
 			}
-			
-				
+			//sorting
 			int tutucu;
-			
-			for(int k = 0; k < loto.length;k++) {
-				for(int b=k+1; b<loto.length;b++) {
-					if(loto[k]<loto[b]) {
-						tutucu=loto[k];
-						loto[k]=loto[b];
-						loto[b]=tutucu;
-					}
+			for (int k = 0; k < loto.length; k++) {
+				
+				for (int b = k + 1; b < loto.length; b++) {
 					
-				}System.out.print(loto[k]+" ");
+					if (loto[k] < loto[b]) {
+						tutucu = loto[k];
+						loto[k] = loto[b];
+						loto[b] = tutucu;
+					}
+
+				}
+				System.out.print(loto[k] + " ");
 			}
 			System.out.println();
 		}
